4c9a8808ba91d39f4e4a4b507419f4125979766c
amir <amirkhakshour@gmail.com>
2020-10-20 16:09:47 +0200

    make grouper simpler

--- mapreduce/grouper.py
+++ mapreduce/grouper.py
@@ -1,4 +1,88 @@
+import pprint
 from collections import defaultdict
+
+
+def get_or_create_brand(product, catalog):
+    # add brand if not exists
+    if product['brand'] not in catalog:
+        catalog[product['brand']] = dict()
+    return catalog.get(product['brand'])
+
+
+def get_or_create_article(product, root):
+    # add article number if not exists
+    if product['article_number'] not in root:
+        root[product['article_number']] = dict(
+            article_number=product['article_number'],
+            variations=list(),
+        )
+    return root.get(product['article_number'])
+
+
+def add_product_to_article(product, article):
+    if '_default_attrs' not in article:
+        article['_default_attrs'] = product
+
+    product_diff_article = dict()
+    for attr, value in product.items():
+        if attr not in article['_default_attrs'] or \
+                article['_default_attrs'][attr] != value:
+            product_diff_article[attr] = value
+
+    article_diff = dict()
+    for attr, value in dict(article['_default_attrs']).items():
+        if attr in product_diff_article:
+            article_diff[attr] = value
+            del article['_default_attrs'][attr]
+
+    if article_diff and not article['variations']:
+        article['variations'].append(article_diff)
+
+    new_variation = product_diff_article
+
+    # add diff_attrs to already variations
+    variation_is_new = True
+    for var in article['variations']:
+        var.update(article_diff)
+        if new_variation == var:
+            variation_is_new = False
+
+    if variation_is_new:
+        article['variations'].append(new_variation)
+
+
+def add_product_to_brand(product, brand):
+    if '_default_attrs' not in brand:
+        brand['_default_attrs'] = product
+
+    brand_diff = dict()
+    # find new variation by comparing product and brand default attrs
+    product_diff_brand = dict()
+    for attr, value in product.items():
+        if attr not in brand['_default_attrs'] or \
+                brand['_default_attrs'][attr] != value:
+            product_diff_brand[attr] = value
+
+    for attr, value in dict(brand['_default_attrs']).items():
+        if attr in product_diff_brand:
+            brand_diff[attr] = value
+            del brand['_default_attrs'][attr]
+
+    if brand_diff:
+        # move brand diff to article default values
+        for _article_no, _article in brand.items():
+            if _article_no != '_default_attrs' and '_default_attrs' in _article:
+                _article['_default_attrs'].update(
+                    brand_diff
+                )
+
+    article = get_or_create_article(product, brand)
+    add_product_to_article(product_diff_brand, article)
+
+
+def group_product(product, catalog):
+    brand = get_or_create_brand(product, catalog)
+    add_product_to_brand(product, brand)
 
 
 def group_products(products):
@@ -9,70 +93,7 @@ def group_products(products):
     :param products:
     :return:
     """
-    catalog = dict()
-    variation_attrs = defaultdict(list)
+    catalog = defaultdict(dict)
     for product in products:
-        catalog.setdefault(
-            product['brand'], dict()
-        )
-        brand_branch = catalog[product['brand']]
-        brand_branch.setdefault(
-            product['article_number'], dict(
-                article_number=product['article_number'],
-                variations=list(),
-            )
-        )
-        product_branch = brand_branch[product['article_number']]
-        if 'default_attrs' not in product_branch:
-            product_branch['default_attrs'] = product
-        else:
-            """
-            Our strategy:
-            1- for each product attribute, check the values with the default 
-            values that belongs to that article number, if any value is 
-            different than the default one then:
-             1-1: add the attr to variation_attrs list of that article number, 
-             1-2: remove those attributes from default_attrs of that article 
-                number
-             1-3: add those attributes to each variations of 
-             that article number
-             1-4: build `new_variation` from current processing product and 
-             list of variation_attrs and if this `new_variation` is unique 
-             amongst other variations, add it as a new variation 
-            """
-            default_attrs = dict(product_branch['default_attrs'])
-            new_diff_list = list()
-            for attr, value in default_attrs.items():
-                if product[attr] != value:
-                    # remove variation from default values and add as variation
-                    variation_attrs[product['article_number']].append(attr)
-                    new_diff_list.append(attr)
-                    del product_branch['default_attrs'][attr]
-
-            removed_from_default = {
-                attr: default_attrs[attr] for attr in
-                new_diff_list
-            }
-            new_variation = {attr: product[attr] for attr in
-                             variation_attrs[product['article_number']]}
-            if new_variation:
-                variations = product_branch['variations']
-                if not variations:
-                    # if the article is the first variation
-                    variations.append(removed_from_default)
-                else:
-                    # we should add diff set to old variations
-                    for var in variations:
-                        var.update(removed_from_default)
-                # check if the new variation is new if so, add it as a new
-                # variation
-                var_is_different = dict()
-                for _var_id, var in enumerate(variations):
-                    var_is_different[_var_id] = False
-                    for attr, value in new_variation.items():
-                        if var[attr] != value:
-                            var_is_different[_var_id] = True
-                            continue
-                if all(var_is_different.values()):
-                    variations.append(new_variation)
+        group_product(product, catalog)
     return catalog
